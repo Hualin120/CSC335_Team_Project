3bfadc912c1283a3a2326f2551a97b03db5b1c0b
Hualin120 <hualin120@gmail.com>
2025-10-27 13:21:02 -0400

    update some code to Hualin's branch

--- app.py
+++ app.py
@@ -1,1 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
 
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+
+
+@app.route('/logout')
+
+
+@app.route('/dashboard')
+@login_required
+def
+
+
+@app.route('/profile')
+@login_required
+
+
+@app.route('/update_profile', methods=['GET', 'POST'])
+@login_required
+
+
+@app.route('/logout ')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'sucess_logout')
+    return redirect(url_for('index'))
