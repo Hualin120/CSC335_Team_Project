f53e0bc47ca11f0de416e0e8a37f978326ad338e
Hualin120 <hualin120@gmail.com>
2025-10-29 14:27:41 -0400

    test

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'B00mB00m'
-app.config['MYSQL_DB'] = 'flaskdb'
+app.config['MYSQL_PASSWORD'] = 'QQMK983648574'
+app.config['MYSQL_DB'] = 'club_management'
 
 mysql = MySQL(app)
 
